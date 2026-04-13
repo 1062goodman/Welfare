616608225e80e65e7d4cefa815db63e67010265f
1062goodman <dudqls7984@naver.com>
2026-04-13 10:20:27 +0900

    api 데이터 뽑아오기

---
+++ dataExtraction.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('datakey')
+
+url = "https://apis.data.go.kr/B554287/NationalWelfareInformationsV001/NationalWelfarelistV001"
+
+params = {
+    'serviceKey': api_key,
+    'callTp': 'L',
+    'pageNo': '1',
+    'numOfRows': '10'
+}
+
+try:
+    response = requests.get(url, params=params)
+    
+    print("상태 코드:", response.status_code)
+    print("결과 데이터:\n", response.text)
+
+except Exception as e:
+    print("요청 중 에러 발생:", e)
